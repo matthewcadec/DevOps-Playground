17d2e048178de41a5d11e030a41b2e9509fd905c
matthewcadec <149057535+matthewcadec@users.noreply.github.com>
2023-10-27 15:46:30 +0100

    Create ipsosTesting.py

---
+++ ipsosTesting.py
@@ -0,0 +1,22 @@
+import paramiko
+
+# create ssh client 
+ssh_client = paramiko.SSHClient()
+
+# remote server credentials
+host = "sftp.ops.retailnext.net"
+username = "7775ea64-6e85-11ee-87a5-c000f8cc7100"
+password = "l3CC-CnlLCbAAsAaO3iK-kwx0dE"
+port = 2022
+
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname=host,port=port,username=username,password=password)
+
+ftp = ssh_client.open_sftp()
+files = ftp.listdir()
+
+print("Listing all the files and Directory: ",files)
+
+# close the connection
+ftp.close()
+ssh_client.close()
