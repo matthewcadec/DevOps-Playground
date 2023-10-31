299e0f9b4c184202e40b075e2a9f0c979b027cc7
matthewcadec <149057535+matthewcadec@users.noreply.github.com>
2023-10-31 17:19:53 +0000

    Sooraj Changes

--- ipsosTesting.py
+++ ipsosTesting.py
@@ -5,7 +5,6 @@ ssh_client = paramiko.SSHClient()
 
 # remote server credentials
 host = "sftp.ops.retailnext.net"
-#host = "34.68.22.227"
 username = "7775ea64-6e85-11ee-87a5-c000f8cc7100"
 password = "l3CC-CnlLCbAAsAaO3iK-kwx0dE"
 port = 2022
@@ -14,7 +13,7 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=host,port=port,username=username,password=password)
 
 ftp = ssh_client.open_sftp()
-ftp.get("/custom_exports/phaseeight/*.txt", "\\server_db1\scripts\Footfall\*.txt")
+ftp.get("/custom_exports/phaseeight/*.txt", "\\server_db1\\scripts\\Footfall")
 
 # close the connection
 ftp.close()
